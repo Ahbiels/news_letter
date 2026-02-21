aca19f4e05a96541b5e68b815ac0cdda5a5f1662
Angelo Fidelis <angelo.fidelis.ab@gmail.com>
2026-02-21 15:33:56 -0300

    criando uma rota para puxar os dados de scrapping do golang

--- golang/main.go
+++ golang/main.go
@@ -1,9 +1,24 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
 	"github.com/Ahbiels/news_letter/modules"
 )
 
 func main() {
-	modules.Scrapping()
+	http.HandleFunc("/run", func(w http.ResponseWriter, r *http.Request) {
+		news := modules.Scrapping()
+		w.Header().Set("Content-Type", "application/json")
+		err := json.NewEncoder(w).Encode(news)
+		if err != nil {
+			http.Error(w, "Error to encoder news", http.StatusInternalServerError)
+			return
+		}
+	})
+	fmt.Println("Server Begin...")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
--- golang/modules/handler_functions.go
+++ golang/modules/handler_functions.go
@@ -1,27 +1,17 @@
 package modules
 
 import (
-	"encoding/json"
 	"fmt"
-	"log"
-	"os"
 	"sync"
 	"time"
 )
 
-func handler_err(err error) {
-	if err != nil {
-		log.Fatal(err)
-	}
-}
-
-func Scrapping() {
+func Scrapping() (news []News) {
 	start := time.Now()
 	var (
-		news       []News
-		jobs       = make(chan News)
-		results    = make(chan News)
-		wgWorkers sync.WaitGroup
+		jobs        = make(chan News)
+		results     = make(chan News)
+		wgWorkers   sync.WaitGroup
 		wgProducers sync.WaitGroup
 	)
 
@@ -30,18 +20,18 @@ func Scrapping() {
 			worker(jobs, results)
 		})
 	}
-	
+
 	SitesToScrapping := 2
 
 	wgProducers.Add(SitesToScrapping)
-	go func() { CanalTech(jobs); defer wgProducers.Done()}()
+	go func() { CanalTech(jobs); defer wgProducers.Done() }()
 	go func() { TecnoBlog(jobs); defer wgProducers.Done() }()
 
-	go func(){
+	go func() {
 		wgProducers.Wait()
 		close(jobs)
 	}()
-	go func(){
+	go func() {
 		wgWorkers.Wait()
 		close(results)
 	}()
@@ -50,22 +40,14 @@ func Scrapping() {
 		news = append(news, result)
 	}
 
-	SaveLocal(news, "filename.json")
 	end := time.Since(start)
 	fmt.Println(end)
+	return news
 
 }
 
 func worker(jobs, results chan News) {
 	for job := range jobs {
 		results <- job
-	}
-}
-
-func SaveLocal(news []News, filename string) {
-	toJSON, err := json.Marshal(news)
-	handler_err(err)
-	if err := os.WriteFile(filename, toJSON, 0644); err != nil {
-		log.Fatal(err)
 	}
 }
